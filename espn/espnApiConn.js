cad431176bad4d40dcb9cbceff9f02dc10549924
andy-aguilar <andres.aguilar@flatironschool.com>
2021-08-09 15:17:26 -0500

    Refactor ESPN API interaction

--- espn/espnApiConn.js
+++ espn/espnApiConn.js
@@ -1,8 +1,10 @@
 const {League, Player, PlayerSeason, Season} = require("../db/models/index")
 const {range} = require("../helpers/functions")
+const axios = require('axios');
+
 
 class EspnApiConn {
-    async getLeagueInfo(espnId){
+    static async getLeagueInfo(espnId){
         const [league, created] = await League.findOrCreate({where: {espnId: espnId}})
         const currentYear = new Date().getFullYear()
         let leagueData = await axios.get(`https://fantasy.espn.com/apis/v3/games/ffl/seasons/${currentYear}/segments/0/leagues/${espnId}`, {
@@ -15,6 +17,46 @@ class EspnApiConn {
         league.save()
         return league
     }
+
+    static async getSingleSeasonData(league, year){
+        const [season, createdSeason] = await Season.findOrCreate({where: {year: year, leagueId: league.id}})
+        const url = year >=2018 ? `https://fantasy.espn.com/apis/v3/games/ffl/seasons/${year}/segments/0/leagues/${league.espnId}` : `https://fantasy.espn.com/apis/v3/games/ffl/leagueHistory/${league.espnId}?seasonId=${year}`
+        let leagueData = await axios.get(url, {
+            headers: {
+                cookie: "espn_s2=AECPR9CSh6x6jxSG9ssT27%2B5hFi5Q%2BNu4nI0ufSbt%2FawuGXvFZcsqVngqO%2B%2FyYsI1LQAmNI80Y62RW8fIjr1lgaS57wZV%2B2Jp%2B62UKZeXVIbH%2Fs7Sd8t9jCiL887forlV2%2Bs%2BUG2OsGftcMibUCHCazwgZ5ZrmisHAzozHsRFDOgQUCgi%2FT825x5Ylo%2FT1HNitSzo0IGZzwB%2FJoG9cVcZ0Dx67bdW%2Bv6NgaAa4jtKRu8lSzo6pbUIUweGXQJ9rqjbSgKTsk2ZQYSZJXKTwA4d8JkN%2FA8d3VVVeF%2B0gJnSx2pLG%2FOrIHGZMPzNFrPR9YCNeQ%3D"
+            }
+        })
+        const members = leagueData.data.members
+        const teams = leagueData.data.teams
+        teams.forEach(team => {
+            const owners = members.filter((member) => team.owners.includes(member.id))
+            owners.forEach( async owner => {
+                const name = team.location + " " + team.nickname
+                const [player, createdPlayer] = await Player.findOrCreate({where:{name:owner.displayName}})
+                const [playerSeason, createdPlayerSeason] = await PlayerSeason.findOrCreate({where: {playerId: player.id, seasonId: season.id}})
+                playerSeason.espnId = owner.id
+                playerSeason.teamId = team.id
+                playerSeason.teamName = name
+                const ps = await playerSeason.save()
+            })
+        })
+    }
+
+    static async getRecentSeasons(league){
+        const date = new Date().getFullYear()
+        const size = date - 2018
+        const array = range(size, 2018)
+        
+        array.forEach(year => {
+            this.getSingleSeasonData(league, year)
+        })
+    }
+
+    static async getOldSeason(league, year){
+        console.log(league.espnId)
+    }
+
+
 }
 
 module.exports = EspnApiConn
--- server.js
+++ server.js
@@ -1,4 +1,5 @@
 const express = require("express");
+const EspnApiConn = require("./espn/espnApiConn")
 const {League, Player, PlayerSeason, Season} = require("./db/models/index")
 const bodyParser = require('body-parser');
 const axios = require('axios');
@@ -6,82 +7,11 @@ const axios = require('axios');
 const app = express()
 
 const PORT = 8000
-async function getData() {
 
-    function range(size, startAt=0){
-        return [...Array(size).keys()].map(i => i+startAt)
-    }
-
-
-    const getLeagueInfo = async (espnId) => {
-        const [league, created] = await League.findOrCreate({where: {espnId: espnId}})
-        const currentYear = new Date().getFullYear()
-        let leagueData = await axios.get(`https://fantasy.espn.com/apis/v3/games/ffl/seasons/${currentYear}/segments/0/leagues/${espnId}`, {
-            headers: {
-                cookie: "espn_s2=AECPR9CSh6x6jxSG9ssT27%2B5hFi5Q%2BNu4nI0ufSbt%2FawuGXvFZcsqVngqO%2B%2FyYsI1LQAmNI80Y62RW8fIjr1lgaS57wZV%2B2Jp%2B62UKZeXVIbH%2Fs7Sd8t9jCiL887forlV2%2Bs%2BUG2OsGftcMibUCHCazwgZ5ZrmisHAzozHsRFDOgQUCgi%2FT825x5Ylo%2FT1HNitSzo0IGZzwB%2FJoG9cVcZ0Dx67bdW%2Bv6NgaAa4jtKRu8lSzo6pbUIUweGXQJ9rqjbSgKTsk2ZQYSZJXKTwA4d8JkN%2FA8d3VVVeF%2B0gJnSx2pLG%2FOrIHGZMPzNFrPR9YCNeQ%3D"
-            }
-        })
-        league.active = leagueData.data.status.isActive
-        league.name = leagueData.data.settings.name
-        league.save()
-        return league
-    }
-
-    const league = await getLeagueInfo("104204")
-    
-    const getSingleSeasonData = async (league, year) => {
-        const [season, createdSeason] = await Season.findOrCreate({where: {year: year, leagueId: league.id}})
-        let leagueData = await axios.get(`https://fantasy.espn.com/apis/v3/games/ffl/seasons/${year}/segments/0/leagues/${league.espnId}`, {
-            headers: {
-                cookie: "espn_s2=AECPR9CSh6x6jxSG9ssT27%2B5hFi5Q%2BNu4nI0ufSbt%2FawuGXvFZcsqVngqO%2B%2FyYsI1LQAmNI80Y62RW8fIjr1lgaS57wZV%2B2Jp%2B62UKZeXVIbH%2Fs7Sd8t9jCiL887forlV2%2Bs%2BUG2OsGftcMibUCHCazwgZ5ZrmisHAzozHsRFDOgQUCgi%2FT825x5Ylo%2FT1HNitSzo0IGZzwB%2FJoG9cVcZ0Dx67bdW%2Bv6NgaAa4jtKRu8lSzo6pbUIUweGXQJ9rqjbSgKTsk2ZQYSZJXKTwA4d8JkN%2FA8d3VVVeF%2B0gJnSx2pLG%2FOrIHGZMPzNFrPR9YCNeQ%3D"
-            }
-        })
-        const members = leagueData.data.members
-        const teams = leagueData.data.teams
-
-        console.log("Teams:", teams)
-        teams.forEach(team => {
-            const owners = members.filter((member) => team.owners.includes(member.id))
-            owners.forEach( async owner => {
-                const name = team.location + " " + team.nickname
-                const [player, createdPlayer] = await Player.findOrCreate({where:{name:owner.displayName}})
-                const [playerSeason, createdPlayerSeason] = await PlayerSeason.findOrCreate({where: {playerId: player.id, seasonId: season.id}})
-                playerSeason.espnId = owner.id
-                playerSeason.teamId = team.id
-                playerSeason.teamName = name
-                const ps = await playerSeason.save()
-            })
-        })
-    }
-
-    const getRecentSeasons = () => {
-        const date = new Date().getFullYear()
-        const size = date - 2018
-        const array = range(size, 2018)
-        
-        array.forEach(year => {
-            getSingleSeasonData(league, year)
-        })
-    }
-
-    getRecentSeasons()
-
-    // getSingleSeasonData(league, 2018)
-
-    // try{
-    //     let leagueData = await axios.get("https://fantasy.espn.com/apis/v3/games/ffl/seasons/2021/segments/0/leagues/104204", {
-    //         headers: {
-    //             cookie: "espn_s2=AECPR9CSh6x6jxSG9ssT27%2B5hFi5Q%2BNu4nI0ufSbt%2FawuGXvFZcsqVngqO%2B%2FyYsI1LQAmNI80Y62RW8fIjr1lgaS57wZV%2B2Jp%2B62UKZeXVIbH%2Fs7Sd8t9jCiL887forlV2%2Bs%2BUG2OsGftcMibUCHCazwgZ5ZrmisHAzozHsRFDOgQUCgi%2FT825x5Ylo%2FT1HNitSzo0IGZzwB%2FJoG9cVcZ0Dx67bdW%2Bv6NgaAa4jtKRu8lSzo6pbUIUweGXQJ9rqjbSgKTsk2ZQYSZJXKTwA4d8JkN%2FA8d3VVVeF%2B0gJnSx2pLG%2FOrIHGZMPzNFrPR9YCNeQ%3D"
-    //         }
-    //     })
-    //     console.log(leagueData.data)
-    // } catch{
-    //     console.log("League not found")
-    // }
-
-}
-
-getData()
+const start = async function(){
+    let lg = await League.findOne()
+    EspnApiConn.getOldSeason(lg)
+}()
 
 
 app.listen(PORT, () => { console.log("We are live on " + PORT)})
