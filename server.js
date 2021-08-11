0341cd649018423f9832eca31e435b7a952945ea
andy-aguilar <andres.aguilar@flatironschool.com>
2021-08-11 14:52:59 -0500

    created initial websocket

--- server.js
+++ server.js
@@ -1,23 +1,60 @@
-const express = require("express");
 const EspnApiConn = require("./espn/espnApiConn");
-const ESPNScraper = require("./espn/espnScraper")
-
+const ESPNScraper = require("./espn/espnScraper");
 const {League, Player, PlayerSeason, Season} = require("./db/models/index")
 const bodyParser = require('body-parser');
 
+const app = require("express")();
+const httpServer = require("http").createServer(app);
+const options = {cors: {
+    origin: "http://localhost:3000",
+    methods: ["GET", "POST"],
+    allowedHeaders: ["my-custom-header"],
+    credentials: true
+}};
+const io = require("socket.io")(httpServer, options);
 
-const app = express()
-
-const PORT = 8000
-
-const start = async function(){
-    // let lg = await EspnApiConn.getLeagueInfo("104204")
-    // EspnApiConn.getSeasons(lg)
-    ESPNScraper.getCookie()
-
-    // leagues = await EspnApiConn.getLeagues()
-    // console.log(leagues)
-}()
+const port = process.env.PORT || 4001;
+const index = require("./routes/index");
 
 
-app.listen(PORT, () => { console.log("We are live on " + PORT)})
+app.use(index);
+
+
+
+
+
+let interval;
+
+io.on("connection", (socket) => {
+    console.log("New client connected");
+    timeout = setTimeout(() => getApiAndEmit(socket), 5000);
+
+    socket.on("code", (arg) => console.log(arg))
+    
+    socket.on("disconnect", () => {
+        console.log("Client disconnected");
+        clearInterval(interval);
+    });
+});
+
+
+
+const getApiAndEmit = socket => {
+    socket.emit("FromAPI", "Do the thing!");
+};
+
+httpServer.listen(port, () => console.log(`Listening on port ${port}`));
+
+
+// const start = async function(){
+//     // let lg = await EspnApiConn.getLeagueInfo("104204")
+//     // EspnApiConn.getSeasons(lg)
+//     ESPNScraper.getCookie()
+
+//     // leagues = await EspnApiConn.getLeagues()
+//     // console.log(leagues)
+// }()
+
+
+
+
