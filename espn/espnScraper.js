938ffb1a9f1d5c2cb7422af5779a55f57c621796
andy-aguilar <andres.aguilar@flatironschool.com>
2021-09-05 12:44:13 -0400

    Refactor load ESPN logic into espnScraper class

--- espn/espnScraper.js
+++ espn/espnScraper.js
@@ -2,50 +2,36 @@ const puppeteer = require('puppeteer');
 const fs = require('fs')
 
 class ESPNScraper{
-    static async getCookie(){
+    static async loadEspn () {
         const browser = await puppeteer.launch()
-        const context = await browser.createIncognitoBrowserContext();
-        const page = await context.newPage()
+        const page = await browser.newPage()
+        let loginFrame
+        let success = false
         await page.setViewport({
             width: 1600,
             height: 1200,
             deviceScaleFactor: 1,
         });
 
-        page.once("load", async () => {
+        try{
+            console.log("Starting navigation")
+            await page.goto("https://www.espn.com", {waitUntil: "load", timeout: 240000})
             console.log("loaded")
-
+            await page.waitForSelector("#global-user-trigger", {timeout:120000})
             await page.click("#global-user-trigger")
-            
-            await page.click('[tref="/members/v3_1/login"]');
-            await page.waitForTimeout(500)
+            console.log("Clicked the trigger")
+            await page.waitForSelector('[tref="/members/v3_1/login"]', {timeout:60000})
+            await page.click('[tref="/members/v3_1/login"]')
+            console.log("login clicked")
+            loginFrame = page.frames().find(frame => frame._name === "disneyid-iframe")
+            await loginFrame.waitForSelector('input[type="email"]', {timeout:60000, visible: true})
+            success = true
+        } catch {
+            await page.screenshot({path: "./puppeteer/noTrigger.png"})
+            console.log("NO TRIGGER")
+        }
 
-            // await page.type('input', "aaguil3@gmail.com")
-            const frame = page.frames().find(frame => frame._name === "disneyid-iframe")
-            await frame.type('input[type="email"]', "aaguil3@gmail.com")
-            await frame.type('input[type="password"]', "dalk_GNI0mok!jept")
-            await frame.click('button[type="submit"]')
-            await page.waitForTimeout(4000)
-            const codeFrame = page.frames().find(frame => frame._name === "disneyid-iframe")
-
-            //// this part should wait for response from USER
-
-            
-            const userResponseCode = "183948"
-            
-            await codeFrame.type('input[type="tel"]', userResponseCode)
-            
-            await codeFrame.click('button[type="submit"]')
-            // console.log(input)
-            await page.screenshot({path: "test4.png"})
-            console.log('done')
-        })
-
-        await page.goto('https://www.espn.com');
-
-
-
-    
+        return [success, page, loginFrame]
     }
 }
 
--- server.js
+++ server.js
@@ -24,97 +24,150 @@ app.use(index);
 
 const puppeteer = require('puppeteer');
 
-// ESPNScraper.getCookie()
+const loadEspn = async () => {
+    const [success, page, loginFrame] = await ESPNScraper.loadEspn()
+    if (success){
+        console.log("ESPN loaded")
+        page.screenshot({path:"./puppeteer/readyForLogin.png"})
+    }
+}
+
+loadEspn()
+
+// io.on("connection", async (socket) => {
+//     console.log("New client connected");
+//     const browser = await puppeteer.launch()
+//     const page = await browser.newPage()
+//     let codeFrame
+//     await page.setViewport({
+//         width: 1600,
+//         height: 1200,
+//         deviceScaleFactor: 1,
+//     });
+//     page.once("load", async () => {
+//             socket.emit("load", "Loading Page...")
+//             console.log("loaded")
+//             try{
+//                 await page.waitForSelector("#global-user-trigger", {timeout:60000})
+//                 console.log("found Selector")
+//             } catch{
+//                 await page.screenshot({path: "./puppeteer/noTrigger.png"})
+//                 socket.emit("load", "Something Went Wrong")
+//             }
+//             await page.click("#global-user-trigger")
+//             console.log("Clicked the trigger")
+//   
+//             
+//             await page.click('[tref="/members/v3_1/login"]')
+//             socket.emit("load", "clicked on login")
+//             console.log("login clicked")
+//             const frame = page.frames().find(frame => frame._name === "disneyid-iframe")
+//             await frame.waitForSelector('input[type="email"]', {timeout:0, visible: true})
+//             await frame.type('input[type="email"]', "aaguil3@gmail.com")
+//             await frame.type('input[type="password"]', "xzh3ceu!vzq*mxf7VEK")
+//             await frame.click('button[type="submit"]')
+//             // const [resultTwo] = await Promise.all([
+//             //     frame.click('button[type="submit"]'),
+//             //     page.waitForNavigation({timeout: 0, waitUntil: "load"})
+//             // ]) 
+
+//             socket.emit("load", "submitted login info")
+//             console.log("first form submitted")
+//             codeFrame = page.frames().find(frame => frame._name === "disneyid-iframe")
+//             await codeFrame.waitForTimeout(30000)
+//             if(codeFrame.isDetached()){
+//                 const cookies = await page.cookies()
+//                 const userId = cookies.find(cookie => cookie.name === "SWID")
+//                 const espnS2 = cookies.find(cookie => cookie.name === "espn_s2")
+//                 if(userId && espnS2){
+//                     const userIdString = userId.value.substring(1, 37)
+//                     const espnS2String = espnS2.value
+//                     console.log("ESPNS2=", espnS2String)
+//                     console.log("userId=", userIdString)
+//                     const leagues = await EspnApiConn.getLeagues(espnS2String, userIdString)
+//                     console.log(leagues)
+//                     const leaguesString = JSON.stringify(leagues)
+//                     socket.emit("ready", leaguesString)
+//                 } else {
+//                     console.log("NO COOKIES")
+//                     await page.screenshot({path: "./puppeteer/noCookies.png"})
+//                     const content = await page.content()
+//                     fs.writeFile("./puppeteer/noCookies.html", content, err => {
+//                         if(err){
+//                             console.error(err)
+//                             return
+//                         }
+//                     })
+//                 }
+//             } else {
+
+//                 console.log("No detatch after submit, possibly ready for code")
+//                 await page.screenshot({path: "./puppeteer/noDetatch.png"})
+//                 socket.emit("ready", "Type the code!")
+//             }
+//             //// this part should wait for response from USER
+//     })
+
+//     await page.goto('https://www.espn.com', {waitUntil: 'load', timeout: 0});
+
+//     socket.on("code", async (code) => {
+//         console.log("code received")   
+//         await codeFrame.type('input[type="tel"]', code)
+//         await codeFrame.click('button[type="submit"]')
+//         await codeFrame.waitForTimeout(30000)
+//         if (codeFrame.isDetached()){
+//             await page.screenshot({path: "test4.png"})
+//             const cookies = await page.cookies()
+//             fs.writeFile("./cookies.js", cookies, err => {
+//                 if (err) {
+//                     console.error(err)
+//                     return
+//                 }
+//             })
+
+//                 const userId = cookies.find(cookie => cookie.name === "SWID")
+//                 const espnS2 = cookies.find(cookie => cookie.name === "espn_s2")
+//                 if(userId && espnS2){
+//                     const userIdString = userId.value.substring(1, 37)
+//                     const espnS2String = espnS2.value
+//                     console.log("ESPNS2=", espnS2String)
+//                     console.log("userId=", userIdString)
+//                     const leagues = await EspnApiConn.getLeagues(espnS2String, userIdString)
+//                     console.log(leagues)
+//                     const leaguesString = JSON.stringify(leagues)
+//                     socket.emit("done", leaguesString)
+//                 } else {
+//                     console.log("NO COOKIES")
+//                     await page.screenshot({path: "./puppeteer/noCookies2.png"})
+//                     const content = await page.content()
+//                     fs.writeFile("./puppeteer/noCookies2.html", content, err => {
+//                         if(err){
+//                             console.error(err)
+//                             return
+//                         }
+//                     })
+//                 }
+
+//         } else {
+//             console.log("no Detatch after code entry")
+//         }
+        
+//         // const espnS2 = cookies.find(cookie => (
+//         //     cookie.name === "espn_s2")).value
+//         // const userId = cookies.find(cookie => (
+//         //     cookie.name === "SWID"
+//         // )).value.substring(1,37)
 
 
-io.on("connection", async (socket) => {
-    console.log("New client connected");
-    const browser = await puppeteer.launch()
-    const page = await browser.newPage()
-    let codeFrame
-    await page.setViewport({
-        width: 1600,
-        height: 1200,
-        deviceScaleFactor: 1,
-    });
-    page.once("load", async () => {
-            socket.emit("load", "Loading Page...")
-            console.log("loaded")
-            try{
-                await page.waitForSelector("#global-user-trigger", {timeout:60000})
-                console.log("found Selector")
-            } catch{
-                socket.emit("load", "Something Went Wrong")
-            }
-            await page.click("#global-user-trigger")
-            console.log("Clicked the trigger")
-            // const [result] = await Promise.all([
-            //     page.waitForNavigation({timeout: 0, waitUntil: "load"}),
-            //     page.click('[tref="/members/v3_1/login"]')
-            // ])
-            await page.waitForSelector('[tref="/members/v3_1/login"]', {timeout:0})
-            await page.click('[tref="/members/v3_1/login"]')
-            socket.emit("load", "clicked on login")
-            console.log("login clicked")
-            const frame = page.frames().find(frame => frame._name === "disneyid-iframe")
-            await frame.waitForSelector('input[type="email"]', {timeout:0})
-            await frame.type('input[type="email"]', "aaguil3@gmail.com")
-            await frame.type('input[type="password"]', "dalk_GNI0mok!jept")
-            await frame.click('button[type="submit"]')
-            // const [resultTwo] = await Promise.all([
-            //     frame.click('button[type="submit"]'),
-            //     page.waitForNavigation({timeout: 0, waitUntil: "load"})
-            // ]) 
-
-            socket.emit("load", "submitted login info")
-            console.log("first form submitted")
-            codeFrame = page.frames().find(frame => frame._name === "disneyid-iframe")
-            if(codeFrame){
-                await page.waitForTimeout(10000)
-                await page.screenshot({path: "afterLoginClick.png"})
-                const content = await page.content()
-                fs.writeFile("./afterLoginClickContent.html", content, err => {
-                    if (err) {
-                        console.error(err)
-                        return
-                    }}
-                )
-                socket.emit("ready", "Type the code!")
-
-            } else {
-                socket.emit("ready", "Something went wrong")
-            }
-            //// this part should wait for response from USER
-    })
-
-    await page.goto('https://www.espn.com', {waitUntil: 'load', timeout: 0});
-
-    socket.on("code", async (code) => {
-        console.log("code received")   
-        await codeFrame.type('input[type="tel"]', code)
-        await codeFrame.click('button[type="submit"]')
-        await page.waitForTimeout(1000)
-        await page.screenshot({path: "test4.png"})
-        const cookies = await page.cookies()
-        const espnS2 = cookies.find(cookie => (
-            cookie.name === "espn_s2")).value
-        const userId = cookies.find(cookie => (
-            cookie.name === "SWID"
-        )).value.substring(1,37)
-
-        console.log("ESPNS2=", espnS2)
-        console.log("userId=", userId)
-        // const leagues = await EspnApiConn.getLeagues(espnS2, userId)
-        // console.log(leagues)
-        // const leaguesJSON = JSON.stringify(leagus)
-        socket.emit("done", "You did it!")
-    })
+//         // const leaguesJSON = JSON.stringify(leagus)
+//         socket.emit("ready", "You did it!")
+//     })
     
-    socket.on("disconnect", async() => {
-        console.log("Client disconnected");
-        await browser.close();
-    });
-});
+//     socket.on("disconnect", async() => {
+//         console.log("Client disconnected");
+//         await browser.close();
+//     });
+// });
 
 
 
